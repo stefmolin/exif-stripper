c39f7554eda8d56564d963ff771142ef32332f18
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-06-08 11:09:01 -0500

    Handle Linux tests when the filesystem doesn't support extended attributes

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -32,12 +32,16 @@ def image_with_exif_data(tmp_path):
 def image_with_metadata(image_with_exif_data):
     """Fixture for an image with metadata."""
     if RUNNING_ON in ['Darwin', 'Linux']:
-        xattr(image_with_exif_data).set(
-            f'{getuser()}.test_extended_attribute'
-            if RUNNING_ON == 'Linux'
-            else 'com.apple.macl',
-            b'\x00',
-        )
+        try:
+            xattr(image_with_exif_data).set(
+                f'{getuser()}.test_extended_attribute'
+                if RUNNING_ON == 'Linux'
+                else 'com.apple.macl',
+                b'\x00',
+            )
+        except OSError:  # pragma: nocover
+            # filesystem does not support extended attributes
+            pass
     return image_with_exif_data
 
 
