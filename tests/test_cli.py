d58d2e57921f7329bea2ae4eba5d6a73fea4304b
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-06-08 10:57:35 -0500

    Try adding user prefix to metadata test fixture on Linux

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,13 +2,15 @@
 
 import platform
 import subprocess
+from getpass import getuser
 
 import pytest
 from PIL import Image
 
 from exif_stripper import cli
 
-RUNNING_ON_WINDOWS = platform.system() == 'Windows'
+RUNNING_ON = platform.system()
+RUNNING_ON_WINDOWS = RUNNING_ON == 'Windows'
 
 if not RUNNING_ON_WINDOWS:
     from xattr import xattr
@@ -29,8 +31,13 @@ def image_with_exif_data(tmp_path):
 @pytest.fixture
 def image_with_metadata(image_with_exif_data):
     """Fixture for an image with metadata."""
-    if not RUNNING_ON_WINDOWS:
-        xattr(image_with_exif_data).set('the.limit.does.not.exist', b'\x00')
+    if RUNNING_ON in ['Darwin', 'Linux']:
+        xattr(image_with_exif_data).set(
+            f'{getuser()}.test_extended_attribute'
+            if RUNNING_ON == 'Linux'
+            else 'com.apple.macl',
+            b'\x00',
+        )
     return image_with_exif_data
 
 
