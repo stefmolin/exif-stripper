32f7b847a2c9ceb1bc64dba0182cebc41d6e277a
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-10-06 21:23:37 -0400

    Restructure package and expand usage options (#46)

--- src/exif_stripper/__init__.py
+++ src/exif_stripper/__init__.py
@@ -1,3 +1,50 @@
 """EXIF stripper."""
 
-__version__ = '0.3.1'
+from __future__ import annotations
+
+import os
+import platform
+
+from PIL import Image, UnidentifiedImageError
+
+__version__ = '0.4.0'
+
+
+def process_image(filename: str | os.PathLike) -> bool:
+    """
+    Process image metadata.
+
+    Parameters
+    ----------
+    filename : str | os.PathLike
+        The image file to check.
+
+    Returns
+    -------
+    bool
+        Indicator of whether metadata was stripped.
+    """
+    has_changed = False
+    try:
+        # remove EXIF data
+        with Image.open(filename) as im:
+            if exif := im.getexif():
+                exif.clear()
+                im.save(filename)
+                has_changed = True
+    except (FileNotFoundError, UnidentifiedImageError):
+        pass  # not an image
+    else:
+        # remove extended attributes (Unix only)
+        if platform.system() != 'Windows':
+            from xattr import xattr
+
+            xattr_obj = xattr(filename)
+            if xattr_obj.list():
+                xattr_obj.clear()
+                has_changed = True
+
+    if has_changed:
+        print(f'Stripped metadata from {filename}')
+
+    return has_changed
--- src/exif_stripper/cli.py
+++ src/exif_stripper/cli.py
@@ -3,55 +3,11 @@
 from __future__ import annotations
 
 import argparse
-import os
-import platform
 from typing import Sequence
 
-from PIL import Image, UnidentifiedImageError
-
-from . import __version__
+from . import __version__, process_image
 
 PROG = 'strip-exif'
-
-
-def process_image(filename: str | os.PathLike) -> bool:
-    """
-    Process image metadata.
-
-    Parameters
-    ----------
-    filename : str | os.PathLike
-        The image file to check.
-
-    Returns
-    -------
-    bool
-        Indicator of whether metadata was stripped.
-    """
-    has_changed = False
-    try:
-        # remove EXIF data
-        with Image.open(filename) as im:
-            if exif := im.getexif():
-                exif.clear()
-                im.save(filename)
-                has_changed = True
-    except (FileNotFoundError, UnidentifiedImageError):
-        pass  # not an image
-    else:
-        # remove extended attributes (Unix only)
-        if platform.system() != 'Windows':
-            from xattr import xattr
-
-            xattr_obj = xattr(filename)
-            if xattr_obj.list():
-                xattr_obj.clear()
-                has_changed = True
-
-    if has_changed:
-        print(f'Stripped metadata from {filename}')
-
-    return has_changed
 
 
 def main(argv: Sequence[str] | None = None) -> int:
