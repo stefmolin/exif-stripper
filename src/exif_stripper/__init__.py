236abef4a467738b69e512b260e2740d06f894e3
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-11-13 07:55:14 -0500

    Remove extended attributes logic since that isn't on the file itself

--- src/exif_stripper/__init__.py
+++ src/exif_stripper/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import os
-import platform
+from contextlib import suppress
 
 from PIL import Image, UnidentifiedImageError
 
@@ -12,7 +12,7 @@ __version__ = '0.5.1'
 
 def process_image(filename: str | os.PathLike) -> bool:
     """
-    Process image metadata.
+    Process image EXIF metadata.
 
     Parameters
     ----------
@@ -24,43 +24,12 @@ def process_image(filename: str | os.PathLike) -> bool:
     bool
         Indicator of whether metadata was stripped.
     """
-    has_changed = False
-    try:
-        # remove EXIF data
-        with Image.open(filename) as im:
-            if exif := im.getexif():
-                exif.clear()
-                im.save(filename)
-                has_changed = True
-    except (FileNotFoundError, UnidentifiedImageError):
-        pass  # not an image
-    else:
-        # remove extended attributes (Unix only)
-        if platform.system() != 'Windows':
-            import warnings
-
-            from xattr import xattr
-
-            xattr_obj = xattr(filename)
-            xattr_list = xattr_obj.list()
-
-            if xattr_list:
-                original_xattr_list = xattr_list[:]
-
-                xattr_obj.clear()
-
-                xattr_obj = xattr(filename)
-                xattr_list = xattr_obj.list()
-
-                has_changed |= set(xattr_list) != set(original_xattr_list)
-                if xattr_list:
-                    xattr_list_str = ', '.join(xattr_list)
-                    warnings.warn(
-                        f'Extended attributes {xattr_list_str} in {filename} cannot be removed.',
-                        stacklevel=2,
-                    )
-
-    if has_changed:
-        print(f'Stripped metadata from {filename}')
-
-    return has_changed
+    with suppress(FileNotFoundError, UnidentifiedImageError), Image.open(
+        filename
+    ) as im:
+        if exif := im.getexif():
+            exif.clear()
+            im.save(filename)
+            print(f'Stripped EXIF metadata from {filename}')
+            return True
+    return False
